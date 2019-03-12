6f88f16ebc88d7227d5f03b8e622357c03984d66
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-03-12 15:59:39 +0700

    Add singleton pattern

---
+++ test.py
@@ -0,0 +1,23 @@
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if not Singleton.__instance:
+            print(" __init__ method called..")
+        else:
+            print("Instance already created:", self.getInstance())
+
+    @classmethod
+    def getInstance(cls):
+        if not cls.__instance:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+
+s = Singleton()  # class initialized, but object not created
+print('------------------')
+print(s)
+print('------------------')
+print("Object created", Singleton.getInstance())  # Object gets created here
+print('------------------')
+s1 = Singleton()  # instance already created
